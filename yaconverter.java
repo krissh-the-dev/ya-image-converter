533484c1893afb84573bd4f9a3271deb5ec2fd5d
AsarMohammed <asarjamal35@gmail.com>
2019-10-02 17:58:39 +0530

    added action handler

--- yaconverter.java
+++ yaconverter.java
@@ -1,7 +1,9 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 class yaconverter {
+  protected static JTextField tf;
     public static void main(String args[]) {
 
         //Creating the Frame
@@ -13,16 +15,16 @@ class yaconverter {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Creating the MenuBar and adding components
         JMenuBar mb = new JMenuBar();
-        JMenu m1 = new JMenu("Close");
+        JMenu m1 = new JMenu("About");
         mb.add(m1);
 
         //Creating the panel at bottom and adding components
         JPanel panel = new JPanel(); // the panel is not visible in output
-        JLabel label = new JLabel("Enter File Location");
-        JTextField tf = new JTextField(25); // accepts upto 100 characters
-        JButton gotofiles = new JButton("Go to files");
+        JLabel label = new JLabel("File path");
+        tf  = new JTextField(25); // accepts upto 100 characters
+        JButton gotofiles = new JButton("Browse");
         JButton convertintobw = new JButton("Convert into B/W ");
-        JButton convertintogs=new JButton("Convert into GreyScale");
+        JButton convertintogs=new JButton("Convert into GrayScale");
         panel.setLayout(new FlowLayout());
         panel.add(label); // Components Added using Flow Layout
         panel.add(tf);
@@ -33,12 +35,16 @@ class yaconverter {
         //Adding Components to the frame.
         frame.getContentPane().add(panel,BorderLayout.CENTER);
         frame.getContentPane().add(mb,BorderLayout.NORTH);
-      /*  frame.getContentPane().add(label);
-        frame.getContentPane().add(tf);
-        frame.getContentPane().add(gotofiles);
-        frame.getContentPane().add(convertintobw);
-        frame.getContentPane().add(convertintogs);*/
-      //  frame.add(panel,BorderLayout)
+        convertintobw.addActionListener(new ButtonListener());
+        convertintogs.addActionListener(new ButtonListener());
+      //  convertintobw.addActionListener(new ButtonListener());
       frame.setVisible(true);
     }
+}
+class ButtonListener extends yaconverter implements ActionListener
+{
+  public void actionPerformed(ActionEvent ae)
+  {
+    tf.setText("OK");
+  }
 }
