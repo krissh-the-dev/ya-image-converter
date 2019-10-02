08a1c2031fa53e50bb21f8c527376295ad7af440
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2019-10-02 17:29:30 +0530

    added converters as package

---
+++ yac/toBW.java
@@ -0,0 +1,30 @@
+package yac;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class toBW {
+  toBW(String path) {
+    try {
+        File input = new File(path);
+        BufferedImage image = ImageIO.read(input);
+
+        BufferedImage result = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_BINARY);
+
+        Graphics2D graphic = result.createGraphics();
+        graphic.drawImage(image, 0, 0, Color.WHITE, null);
+        graphic.dispose();
+        // thanks to tutorialspoint for conversion algorithm
+
+        File output = new File("outputs\\" + path + "-bw.jpeg");
+        ImageIO.write(result, "jpeg", output);
+    } catch (IOException e) {
+        e.printStackTrace();
+    } catch(ArrayIndexOutOfBoundsException aob) {
+        System.out.println("Please provide command-line arguments.");
+    }
+  }
+}
---
+++ yac/toGrayscale.java
@@ -0,0 +1,52 @@
+package yac;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class toGrayscale {
+  toGrayscale(String path) {
+    BufferedImage img = null;
+    File colored = null;
+
+    try{
+      colored = new File(path);
+      img = ImageIO.read(colored);
+    } catch(IOException e){
+        System.out.println(e);
+    } catch (ArrayIndexOutOfBoundsException aoe) {
+        System.out.println("Please give command-line arguments.");
+    }
+
+    int width = img.getWidth();
+    int height = img.getHeight();
+
+    for(int y = 0; y < height; y++){
+      for(int x = 0; x < width; x++){
+        int p = img.getRGB(x,y);
+
+        int a = (p>>24)&0xff;
+        int r = (p>>16)&0xff;
+        int g = (p>>8)&0xff;
+        int b = p&0xff;
+
+        int avg = (r+g+b)/3;
+
+        p = (a<<24) | (avg<<16) | (avg<<8) | avg;
+
+        img.setRGB(x, y, p);
+      }
+    }
+
+    // thanks to stackoverflow for image conversion algorithm
+
+    try{
+      colored = new File("outputs\\" + path + "-grayscale.jpg");
+      ImageIO.write(img, "jpg", colored);
+    }catch(IOException e){
+      System.out.println(e);
+    }
+  }
+}
