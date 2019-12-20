19b26a9a4c9cf0aaeed6be001be7610ad2fc7152
AsarMohammed <asarjamal35@gmail.com>
2019-12-20 11:00:05 +0530

    Browse Icon bug fix

--- yaconverter-beta-a.java
+++ yaconverter-beta-a.java
@@ -1,64 +1,142 @@
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.Dimension;
-import javax.imageio.ImageIO;
 import java.awt.image.*;
-import javax.swing.JFileChooser;
-class yaconverterasarcopy {
-  protected static JTextField tf;
-    public static void main(String args[]) {
+import javax.swing.filechooser.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import yac.toBW;
+import yac.toGrayscale;
 
-        //Creating the Frame
-        JFrame frame = new JFrame("Ya-Converter");
-  ImageIcon ic = new ImageIcon("logo.png");
-  JDesktopPane dp = new JDesktopPane();
-  JLabel lbl = new JLabel(ic);
-  JPanel transparentPanel = new JPanel();
-      //  BufferedImage img = ImageIO.read(new File("C:\\Users\\asarj\\Downloads\\YA-logo-white.png"));
+class yaconverterA{
+  static JFrame frame = new JFrame("Ya Converter");
+  static JTextField pathField;
+  static JLabel status, img;
+  static JButton tobw, togs, browse;
+  static JPanel imageViewer = new JPanel();
+    public static void main(String[] args) {
+      // Theming
+      try {
+        UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+      } catch(Exception looke) {
+        System.out.println("Look and feel error.");
+      }
 
-        //ImageIcon icon = new ImageIcon(img);
-
-
-        //ImageIcon image = new ImageIcon("C:\\Users\\asarj\\Downloads\\YA-logo-white.png");
-        //JLabel label = new JLabel(image);
-        //frame.add(image);
-        frame.setSize(400, 400);
-        frame.setResizable(true);
+      // Frame settings
+        frame.setSize(550, 670);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //Creating the MenuBar and adding components
+        // Menu bar
         JMenuBar mb = new JMenuBar();
         JMenu m1 = new JMenu("About");
         mb.add(m1);
 
-        //Creating the panel at bottom and adding components
-        JPanel panel = new JPanel(); // the panel is not visible in output
-        JLabel label = new JLabel("File path");
-        tf  = new JTextField(25); // accepts upto 25 characters
-        JButton browse = new JButton("Browse");
-        JButton convertintobw = new JButton("Convert into B/W ");
-        JButton convertintogs=new JButton("Convert into GrayScale");
-        panel.setLayout(new FlowLayout());
-        panel.add(new JLabel(ic));
-        panel.add(label); // Components Added using Flow Layout
-        panel.add(tf);
-        panel.add(browse);
-        panel.add(convertintobw);
-        panel.add(convertintogs);
+        // Panels
+        JPanel mainPanel = new JPanel();
+        JPanel statusBar = new JPanel();
+
+        ImageIcon ic = new ImageIcon("raw\\logo.png");
+        frame.setIconImage(ic.getImage());
+        JLabel label = new JLabel("File path: ");
+        pathField = new JTextField(45);
+        browse = new JButton("Browse");
+        tobw = new JButton("Convert into B/W");
+        togs = new JButton("Convert into GrayScale");
+
+
+        // Image Viewer
+        imageViewer.setSize(300, 300);
+        img = new JLabel(ic);
+        img.setSize(50, 50);
+        imageViewer.add(img);
+
+
+        // Main panel
+        mainPanel.setLayout(new FlowLayout());
+        mainPanel.add(label);
+        mainPanel.add(pathField);
+        mainPanel.add(browse);
+        mainPanel.add(tobw);
+        mainPanel.add(togs);
+
+        // Status Bar
+        statusBar.setLayout(new FlowLayout(FlowLayout.LEFT));
+        status = new JLabel("Ready");
+
+        statusBar.add(status);
+
+
         frame.setLayout(new BorderLayout());
-        //Adding Components to the frame.
-        frame.getContentPane().add(panel,BorderLayout.CENTER);
-        frame.getContentPane().add(mb,BorderLayout.NORTH);
-        convertintobw.addActionListener(new ButtonListener());
-        convertintogs.addActionListener(new ButtonListener());
 
+        // Action Handling
+        tobw.addActionListener(new ButtonListener());
+        togs.addActionListener(new ButtonListener());
+        browse.addActionListener(new ButtonListener());
 
+        // Adding to frame
+        frame.getContentPane().add(mb, BorderLayout.NORTH);
+        frame.getContentPane().add(imageViewer, BorderLayout.NORTH);
+        frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
+        frame.getContentPane().add(statusBar, BorderLayout.SOUTH);
 
-      frame.setVisible(true);
-
-
-
-
+        frame.setVisible(true);
+    }
 }
+
+class ButtonListener extends yaconverter implements ActionListener {
+  public void actionPerformed(ActionEvent ae) {
+    String path = pathField.getText();
+    JFileChooser bb = null;
+    int bbd = 0;
+    if(ae.getSource() == browse) {
+      bb = new JFileChooser(FileSystemView.getFileSystemView());
+      bbd = bb.showOpenDialog(frame);
+
+      if (bbd == JFileChooser.APPROVE_OPTION) {
+          pathField.setText(bb.getSelectedFile().getAbsolutePath());
+          path = pathField.getText();
+      }
+    }
+
+    if (path.equals("")){
+      status.setText("Choose a file to convert.");
+      return;
+    }
+    if (!(new File(path).exists()))
+      status.setText("File does not exist!");
+
+    try {
+      File op = null;
+      String conv = "";
+      BufferedImage image = ImageIO.read(new File(path));
+      if (ae.getSource() == tobw) {
+        op = toBW.convert(path);
+        conv = path + "-bw.jpeg";
+        status.setText("Converted to black and white.");
+      }
+      else if (ae.getSource() == togs) {
+        op =toGrayscale.convert(path);
+        conv = path + "-grayscale.jpeg";
+        status.setText("Converted to grayscale.");
+      }
+      else {}
+
+      if (op == null)
+        status.setText("File chosen successfully.");
+      else{
+        imageViewer.remove(img);
+        img = new JLabel(new ImageIcon(conv));
+        imageViewer.add(img);
+        frame.getContentPane().add(imageViewer, BorderLayout.NORTH);
+        status.setText("Image saved as " + conv + ".");
+      }
+    } catch(javax.imageio.IIOException iioe) {
+      status.setText("File not found.");
+    } catch (Exception e) {
+      status.setText("Error converting the file. The file is either not an image or currupted.");
+    }
+  }
 }
